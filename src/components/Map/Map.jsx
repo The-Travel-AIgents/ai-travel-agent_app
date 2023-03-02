279eb143ae10777bf400db12f3a45228abb3ede3
AustinRado <radoaussy@gmail.com>
2023-03-02 11:15:19 +0300

    first commit

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-const Map =()=>{
+import useStyles from './styles.js'
+/*import { GoogleMap } from '@react-google-maps/api';*/
+
+const Map = () => {
+    const classes = useStyles();
+    const isMoblie = useMediaQuery('(min-width:600px)');
+    
+    const coordinates = {lat:0, lng:0};
+
     return(
-        <div>
-            <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyC_94BWWYHs2Y_0YRFBNmc8OU7mkrLIxFA'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''} /* important when you change the map */
+                onChildClick={''}
+            >
+            </GoogleMapReact>
         </div>
         );
 }
