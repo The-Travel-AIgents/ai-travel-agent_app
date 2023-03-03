a2eb0077327b2aea8d2e51cbabb6d03f1a997a2d
Quyum Kehinde <quyumkehinde@gmail.com>
2023-03-03 08:26:42 +0100

    fix: hyperlink

--- src/Layout.js
+++ src/Layout.js
@@ -20,7 +20,7 @@ const Layout = (props) => (
                 </button>
             </div>
             <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <a href="{{ route('voteIndex') }}" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Try it out! </a>
+                <Link to="/app" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Try it out! </Link>
             </div>
             </div>
         </div>
