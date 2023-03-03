f553be813e3644df3072561923084aa3530ed95b
Quyum Kehinde <49095691+quyumkehinde@users.noreply.github.com>
2023-03-03 22:25:03 +0100

    fix: home (#3)

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from "./image/logo.png"
 
 const Layout = (props) => (
   <>
@@ -8,7 +9,7 @@ const Layout = (props) => (
             <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div class="flex justify-start lg:w-0 lg:flex-1">
                 <Link to="/">
-                    <span className="text-lg">Travel AIgent</span>
+                    <img alt="Logo" src={logo} className="h-10 w-auto" />
                 </Link>
             </div>
             <div class="-mr-2 -my-2 md:hidden">
@@ -29,7 +30,7 @@ const Layout = (props) => (
             <div class="pt-5 pb-6 px-5">
                 <div class="flex items-center justify-between">
                 <div>
-                    <img class="h-8 w-auto" src="{{ asset('/storage/images/logo.png') }}" alt="Workflow" />
+                    <img class="h-8 w-auto" src={logo} alt="Workflow" />
                 </div>
                 <div class="-mr-2">
                     <button type="button" id="close-menu-btn" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -43,15 +44,7 @@ const Layout = (props) => (
             </div>
             <div class="py-6 px-5 space-y-6">
                 <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-                    <a href="{{ route('home') }}" class="text-base font-medium text-gray-900 hover:text-gray-700"> Home </a>
-                    <a href="{{ route('resultIndex') }}" class="text-base font-medium text-gray-900 hover:text-gray-700"> Election Result </a>
-                </div>
-                <div>
-                    <a href="{{ route('voteIndex') }}" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Vote </a>
-                      <p class="mt-6 text-center text-base font-medium text-gray-500">
-                          Are you an admin?
-                          <a href="{{ route('login') }}" class="text-indigo-600 hover:text-indigo-500"> Sign in </a>
-                      </p>
+                   <Link to="/app" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Try it out! </Link>
                 </div>
             </div>
             </div>
